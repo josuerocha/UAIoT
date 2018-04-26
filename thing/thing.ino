1f3ed4988f535235e2528218e2c51c5f29895ae0
josuerocha <josuerocha@me.com>
2018-04-26 18:15:31 -0300

    [MAJOR] GSM HTTP post communication done

--- thing/thing.ino
+++ thing/thing.ino
@@ -1,28 +1,18 @@
-#include<SoftwareSerial.h>
 
-SoftwareSerial client(2,3);
+#include <ArduinoJson.h>
+#include <Http.h>
 
 
-String reading="{  \" : 1,  \"latitude\" : 12.983027,  \"longitude\" : 80.2594,  \"timestamp\" : 100,  }";
+unsigned int RX_PIN = 11;
+unsigned int TX_PIN = 10;
+unsigned int RST_PIN = 9;
 
-void setup()
-{
+
+void setup(){
+
   Serial.begin(9600);
-  client.begin(9600);
-  delay(500);
-
-  if(client.available())
-  {
-    Serial.print("Connected");
-  }
-  else
-  {
-    Serial.print("NotConnected");
-  }
-
-  //initSIM();
-  connectGPRS();
-  connectHTTP();
+  
+  sendRequest();
 }
 
 void loop()
@@ -30,73 +20,17 @@ void loop()
 
 }
 
+void sendRequest(){
+  HTTP http(9600, RX_PIN, TX_PIN, RST_PIN);
+  http.configureBearer("movistar.es"); 
+  http.connect();
 
+  char response[256];
+  Result result = http.post("http://uaiot-server.herokuapp.com/serv/log", "{\"latitude\" : -19.936517,  \"longitude\" : -43.978054 , \"thing\": { \"id\" : \"123412\"  }}", response);
 
-void connectGPRS()
-{ 
-  client.println("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");
-  delay(1000);
-  ShowSerialData();
+  Serial.println(response);
 
-  client.println("AT+SAPBR=3,1,\"APN\",\"www\"");//APN
-  delay(1000);
-  ShowSerialData();
-
-  client.println("AT+SAPBR=1,1");
-  delay(1000);
-  ShowSerialData();
-
-  client.println("AT+SAPBR=2,1");
-  delay(1000);
-  ShowSerialData();
+  http.disconnect();
 }
 
-void connectHTTP()
-{
-  client.println("AT+HTTPINIT");
-  delay(1000);
-  ShowSerialData();
 
-  client.println("AT+HTTPPARA=\"CID\",1");
-  delay(1000);
-  ShowSerialData();
-
-  client.println("AT+HTTPPARA=\"URL\",\"http://xxx.xxx.xx/Listener/\"");//Public server IP address
-  client.println("AT+HTTPPARA=\"URL\",\"http://testServer/Listener/\"");//Public server address
-  delay(1000);
-  ShowSerialData();
-
-  client.println("AT+HTTPPARA=\"CONTENT\",\"application/json\"");
-  delay(1000);
-  ShowSerialData();
-
-
-  client.println("AT+HTTPDATA=" + String(reading.length()) + ",100000");
-  delay(1000);
-  ShowSerialData();
-
-  client.println(reading);
-  delay(1000);
-  ShowSerialData;
-
-  client.println("AT+HTTPACTION=1");
-  delay(1000);
-  ShowSerialData();
-
-  client.println("AT+HTTPREAD");
-  delay(1000);
-  ShowSerialData();
-
-  client.println("AT+HTTPTERM");
-  delay(1000);
-  ShowSerialData;
-}
-
-void ShowSerialData()
-{
-  while(client.available()!=0)
-  {
-  Serial.write(client.read());
-  delay(100);
-  }
-}
